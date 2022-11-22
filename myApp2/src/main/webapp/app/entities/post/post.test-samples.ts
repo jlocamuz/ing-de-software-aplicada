04561df062874961a20c65fa79fd43afe31fbd69
jlocamuz <jlocamuz@gmail.com>
2022-11-22 17:15:47 -0300

    Initial version of blog generated by generator-jhipster@7.9.3

---
+++ myApp2/src/main/webapp/app/entities/post/post.test-samples.ts
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs/esm';
+
+import { IPost, NewPost } from './post.model';
+
+export const sampleWithRequiredData: IPost = {
+  id: 35989,
+  title: 'Fantástico',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2022-11-22T05:50'),
+};
+
+export const sampleWithPartialData: IPost = {
+  id: 93202,
+  title: 'Serbia',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2022-11-21T22:03'),
+};
+
+export const sampleWithFullData: IPost = {
+  id: 23240,
+  title: 'recontextualize virtual copying',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2022-11-21T23:44'),
+};
+
+export const sampleWithNewData: NewPost = {
+  title: 'withdrawal',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2022-11-22T01:15'),
+  id: null,
+};
+
+Object.freeze(sampleWithNewData);
+Object.freeze(sampleWithRequiredData);
+Object.freeze(sampleWithPartialData);
+Object.freeze(sampleWithFullData);
